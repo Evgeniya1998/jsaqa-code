fe202d7e412c7b53f9f6038a1e91f8ea4d445b62
Evgesha <Slav.novikova2014@gmail.com>
2023-11-26 21:59:23 +0700

    add docs

--- 7.3/playwright/e2e/UserTest.spec.js
+++ 7.3/playwright/e2e/UserTest.spec.js
@@ -10,12 +10,15 @@ test('successful authorization', async ({ page }) => {
   await page.getByPlaceholder("Email").fill(login);
   await page.getByPlaceholder("Пароль").click();
   await page.getByPlaceholder("Пароль").fill(password);
-  await page.getByTestId("login-submit-btn").click();
+  await page.getByTestId("login-submit-btn").click();  
+  await page.screenshot({ path: 'screenshot.png' });
   await expect(page).toHaveURL("https://netology.ru/profile");
+  await page.screenshot({ path: 'screenshots.png' });
 });
 test("unsuccessful authorization", async ({ page }) => {
   test.slow();
   await page.goto("https://netology.ru/?modal=sign_in");
+  
   await expect(page).toHaveTitle(
     "Нетология — обучение современным профессиям онлайн"
   );
@@ -25,7 +28,7 @@ test("unsuccessful authorization", async ({ page }) => {
   await page.getByPlaceholder("Пароль").fill("Slonnnnnnnnnnn");
   await page.getByTestId("login-submit-btn").click();
   await expect(page.locator("data-testid=login-error-hint")).toHaveText(
-    "Вы ввели неправильно логин или пароль"
-  );
+    "Вы ввели неправильно логин или пароль");
+  await page.screenshot({ path: 'screenshotic.png' });
 }
 );
