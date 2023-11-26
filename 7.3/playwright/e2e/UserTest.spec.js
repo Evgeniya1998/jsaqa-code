d288b29d2ce5877d8b21f4aa69133cea82c7f58d
Evgesha <Slav.novikova2014@gmail.com>
2023-11-26 21:30:52 +0700

    add docs

--- 7.3/playwright/e2e/UserTest.spec.js
+++ 7.3/playwright/e2e/UserTest.spec.js
@@ -1,21 +1,17 @@
-
-const { test, expect } = require("@playwright/test");
-const login = require("./user");
-
-test("successful authorization", async ({ page }) => {
-  //test.slow();
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { login, password } = require("../user");
+test('successful authorization', async ({ page }) => {
   await page.goto('https://netology.ru/?modal=sign_in');
-  await expect(page).toHaveTitle(
-    "Нетология — обучение современным профессиям онлайн"
-  );
+  await expect(page).toHaveTitle("Нетология — обучение современным профессиям онлайн");
+
   await page.getByPlaceholder("Email").click();
-  await page.getByPlaceholder("Email").fill(login());
+  await page.getByPlaceholder("Email").fill(login);
   await page.getByPlaceholder("Пароль").click();
-  await page.getByPlaceholder("Пароль").fill(password());
+  await page.getByPlaceholder("Пароль").fill(password);
   await page.getByTestId("login-submit-btn").click();
   await expect(page).toHaveURL("https://netology.ru/profile");
 });
-
 test("unsuccessful authorization", async ({ page }) => {
   test.slow();
   await page.goto("https://netology.ru/?modal=sign_in");
@@ -23,13 +19,12 @@ test("unsuccessful authorization", async ({ page }) => {
     "Нетология — обучение современным профессиям онлайн"
   );
   await page.getByPlaceholder("Email").click();
-  await page.getByPlaceholder("Email").fill(login());
+  await page.getByPlaceholder("Email").fill(login);
   await page.getByPlaceholder("Пароль").click();
-  await page.getByPlaceholder("Пароль").fill("Slon");
+  await page.getByPlaceholder("Пароль").fill("Slonnnnnnnnnnn");
   await page.getByTestId("login-submit-btn").click();
   await expect(page.locator("data-testid=login-error-hint")).toHaveText(
     "Вы ввели неправильно логин или пароль"
   );
-
 }
 );
